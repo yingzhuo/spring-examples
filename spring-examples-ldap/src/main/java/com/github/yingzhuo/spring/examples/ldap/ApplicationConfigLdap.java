411ecd779d62f141825575f638384874f95fe6e7
Bill Ying <yingzhor@gmail.com>
2016-04-12 16:13:09 +0800

    update

--- spring-examples-ldap/src/main/java/com/github/yingzhuo/spring/examples/ldap/ApplicationConfigLdap.java
+++ spring-examples-ldap/src/main/java/com/github/yingzhuo/spring/examples/ldap/ApplicationConfigLdap.java
@@ -14,12 +14,16 @@ public class ApplicationConfigLdap {
     @Bean
     @ConfigurationProperties(prefix = "spring.ldap.context-source")
     public LdapContextSource ldapContextSource() {
-        return new LdapContextSource();
+        LdapContextSource bean = new LdapContextSource();
+//        bean.setUrl("ldap://10.211.55.13:389");
+//        bean.setBase("ou=people,dc=yingzhuo,dc=org");
+//        bean.setUserDn("cn=admin,dc=yingzhuo,dc=org");
+//        bean.setPassword("133810");
+        return bean;
     }
 
     @Bean
     public LdapTemplate ldapTemplate(LdapContextSource ldapContextSource) {
         return new LdapTemplate(ldapContextSource);
     }
-
 }
