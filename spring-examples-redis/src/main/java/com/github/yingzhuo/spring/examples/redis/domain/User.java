f797fafa46cc3a7ad230d38b4f92d6ec8d15a802
Bill Ying <yingzhor@gmail.com>
2016-05-04 13:33:47 +0800

    update

---
+++ spring-examples-redis/src/main/java/com/github/yingzhuo/spring/examples/redis/domain/User.java
@@ -0,0 +1,26 @@
+package com.github.yingzhuo.spring.examples.redis.domain;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public class User implements Serializable {
+
+    private String id = UUID.randomUUID().toString();
+    private String name;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
