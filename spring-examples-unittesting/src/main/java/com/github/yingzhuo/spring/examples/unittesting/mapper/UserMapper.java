6ba26596ed00d565cdb0d76fd930573085ef316a
yingzhuo <yingzhor@gmail.com>
2016-10-13 08:49:36 +0800

    update

--- spring-examples-unittesting/src/main/java/com/github/yingzhuo/spring/examples/unittesting/mapper/UserMapper.java
+++ spring-examples-unittesting/src/main/java/com/github/yingzhuo/spring/examples/unittesting/mapper/UserMapper.java
@@ -8,7 +8,7 @@ import org.apache.ibatis.annotations.Select;
 @Mapper
 public interface UserMapper {
 
-    @Select("select * from T_USER WHERE id = #{id}")
+    @Select("select * from T_USER where id = #{id}")
     public User findById(@Param("id") String id);
 
 }
