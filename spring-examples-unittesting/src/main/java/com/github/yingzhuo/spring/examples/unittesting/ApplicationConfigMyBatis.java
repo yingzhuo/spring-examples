37b487004e38f35d3307b47fe14d1f5f31ae5299
yingzhuo <yingzhor@gmail.com>
2016-10-16 10:11:36 +0800

    update

--- spring-examples-unittesting/src/main/java/com/github/yingzhuo/spring/examples/unittesting/ApplicationConfigMyBatis.java
+++ spring-examples-unittesting/src/main/java/com/github/yingzhuo/spring/examples/unittesting/ApplicationConfigMyBatis.java
@@ -2,12 +2,20 @@ package com.github.yingzhuo.spring.examples.unittesting;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableTransactionManagement
 @MapperScan(basePackages = {"com.github.yingzhuo.spring.examples.unittesting"}, annotationClass = Mapper.class)
 public class ApplicationConfigMyBatis {
 
+    @Bean
+    public DataSourceTransactionManager dataSourceTransactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
 }
