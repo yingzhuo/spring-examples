54bb008185b397e7eacbe588ec9f0147f258bc15
应卓 <yingzhor@gmail.com>
2017-04-18 17:32:43 +0800

    update

--- spring-examples-email/src/main/java/com/github/yingzhuo/spring/examples/email/ApplicationBoot.java
+++ spring-examples-email/src/main/java/com/github/yingzhuo/spring/examples/email/ApplicationBoot.java
@@ -1,12 +1,25 @@
 package com.github.yingzhuo.spring.examples.email;
 
+import com.github.yingzhuo.spring.examples.email.tool.MailService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class ApplicationBoot {
 
+    @Autowired
+    private MailService mailService;
+
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationBoot.class, args);
+        SpringApplication.run(ApplicationBoot.class, args).close();
     }
+
+    @EventListener(classes = {ContextRefreshedEvent.class})
+    protected void handleEvent() {
+        mailService.sendMimeMessage("yingzhor@163.com", "hello", "<h2>测试一下</h2>");
+    }
+
 }
