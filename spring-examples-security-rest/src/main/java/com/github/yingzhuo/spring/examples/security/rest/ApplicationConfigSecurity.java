82e92e00b4b0454e6af889943391160ba5b857f9
应卓 <yingzhor@gmail.com>
2017-08-21 10:22:28 +0800

    update

--- spring-examples-security-rest/src/main/java/com/github/yingzhuo/spring/examples/security/rest/ApplicationConfigSecurity.java
+++ spring-examples-security-rest/src/main/java/com/github/yingzhuo/spring/examples/security/rest/ApplicationConfigSecurity.java
@@ -3,7 +3,7 @@
  ** / __| '_ \| '__| | '_ \ / _` |_____ / _ \ \/ / _` | '_ ` _ \| '_ \| |/ _ \/ __|
  ** \__ \ |_) | |  | | | | | (_| |_____|  __/>  < (_| | | | | | | |_) | |  __/\__ \
  ** |___/ .__/|_|  |_|_| |_|\__, |      \___/_/\_\__,_|_| |_| |_| .__/|_|\___||___/
- **    |_|                 |___/                               |_|
+ **     |_|                 |___/                               |_|
  **
  **        https://github.com/yingzhuo/spring-examples
  */
@@ -33,10 +33,9 @@ public abstract class ApplicationConfigSecurity {
 
         @Bean
         public UserDetailsService userDetailsService() {
-            return username -> {
-                return new User(username, "123456", true, true, true, true,
+            return username ->
+                new User(username, "123456", true, true, true, true,
                         AuthorityUtils.commaSeparatedStringToAuthorityList("USER,ADMIN"));
-            };
         }
     }
 
